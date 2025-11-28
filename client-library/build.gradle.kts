4c836f16a65184ce1e48d57aa309c9ff119a7edf
peter-szrnka <szrnka.peter@gmail.com>
2025-11-28 19:08:06 +0000

    [ci-skip] Version updated to 0.0.2-SNAPSHOT

--- client-library/build.gradle.kts
+++ client-library/build.gradle.kts
@@ -50,7 +50,7 @@ publishing {
 		register<MavenPublication>("release") {
             groupId = "io.github.easylog"
             artifactId = "android-client-library"
-version = "0.0.1"
+version = "0.0.2-SNAPSHOT"
 
 			afterEvaluate {
                 from(components["release"])
